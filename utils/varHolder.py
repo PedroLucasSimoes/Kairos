2c216301daa2fd3a61a6267b9d1b3618c872f7ae
Pedro Lucas Simões Cabral <lucaspb3012@gmail.com>
2021-11-12 15:57:14 -0300

    Variable Storage

---
+++ utils/varHolder.py
@@ -0,0 +1,9 @@
+atributos = ["Nome", "Idade", "Pontos de Vida", "Sanidade", "Força", "Destreza", "Inteligência", "Vitalidade", "Resistência",
+        "Corrida", "Escalar", "Furtividade", "Salto",
+        "Detectar Mentiras", "Diplomacia", "Disfarce", "Mentir", "Interrogatório", "Lábia", "Liderança",
+        "Armeiro", "Química", "Camuflagem", "Estratégia", "Falsificação", "Primeiros Socorros", "Medicina", "Rastreamento",
+        "Sobrevivência", "Venefício", "Encontrar", "Dirigir",
+        "Acrobacia", "Arremessar", "Briga", "Sacar Rápido", "Esquiva", "Bloqueio",
+        "Espingarda", "Pistola", "Metralhadora", "Armas Elétricas", "Machado", "Facas", "Espadas", "Espadas Curtas", "Bastão", "Arco"]
+
+servers = ["1962", "Ceia de Sangue"]
