3c0147cf90e52e0ebec52c5b9f863d509ee676df
Pedro Lucas Simões Cabral <lucaspb3012@gmail.com>
2021-12-05 22:30:47 -0300

    lil fixes here n there ROLL DONE

--- cogs/rpg_1/roll.py
+++ cogs/rpg_1/roll.py
@@ -2,6 +2,7 @@ from logging import StringTemplateStyle
 import traceback
 import disnake
 from disnake import user
+from disnake.colour import Colour
 from disnake.ext import commands
 from disnake.ext.commands import has_permissions, Param
 from discord.ext.forms import Form, ReactionForm, ReactionMenu
@@ -46,19 +47,22 @@ class Roll(commands.Cog):
         else:
             if self.result < atr:
                 return True
-            else: False
+            else: return False
 
     async def answeringInter(self, inter: disnake.ApplicationCommandInteraction, data : int, hasExtra: bool, extra = 0, diceLimit : int = 6):
+        self.Purple = disnake.Colour(0x4521F9)
         if not hasExtra:
             if self.rollCheck(extra, data, diceLimit):
-                await inter.response.send_message(f"Sucesso <- {self.result}{self.resultList}")
+                embedVar = disnake.Embed(title=f"Teste de {self.atrOld}", description=f"Sucesso <- {self.result} = {self.resultList}", colour=self.Purple)
             else:
-                await inter.response.send_message(f"Falha <- {self.result}{self.resultList}")
+                embedVar = disnake.Embed(title=f"Teste de {self.atrOld}", description=f"Falha <- {self.result} = {self.resultList}", colour=self.Purple)
         elif hasExtra:
             if self.rollCheck(extra, data):
-                await inter.response.send_message(f"Sucesso <- {self.result} = {self.resultList} + {extra}")
+                embedVar = disnake.Embed(title=f"Teste de {self.atrOld}", description=f"Sucesso <- {self.result} = {self.resultList} + {extra}", colour=self.Purple)
             else:
-                await inter.response.send_message(f"Falha <- {self.result} = {self.resultList} + {extra}")
+                embedVar = disnake.Embed(title=f"Teste de {self.atrOld}", description=f"Falha <- {self.result} = {self.resultList} + {extra}", colour=self.Purple)
+        embedVar.set_author(name= self.userRolled.name,icon_url= self.userRolled.avatar.url)
+        await inter.response.send_message(embed=embedVar, delete_after=600)
 
     @commands.slash_command(name="roll", description="Rolagem de testes")
     async def roll (self, inter: disnake.ApplicationCommandInteraction,
@@ -70,23 +74,24 @@ class Roll(commands.Cog):
         if usúario == 0:
             self.userRolled: disnake.Member = inter.author
         else:
-            pass
-        print(atributo)
+            self.userRolled : disnake.Member = usúario
         
 
         if self.isValidAtr(atributo):
-            atributo = atributo.strip().replace(" ", "_")
-            print(f"after:{atributo}")
+            self.atrNew, self.atrOld = atributo.strip().replace(" ", "_"), atributo
             try:
-                self.cur.execute(f"SELECT {atributo} FROM users WHERE id = ?", (str(self.userRolled.id),))
+                self.cur.execute(f"SELECT {self.atrNew} FROM users WHERE id = ?", (str(self.userRolled.id),))
                 data = self.cur.fetchall()[0][0]
-                pprint(data)
+            except IndexError:
+                embedVar = disnake.Embed(title=f"O usúario {self.userRolled.name} não está cadastrado.", description="Caso ache que isso seja um erro, contate Farrys.")
+                embedVar.set_author(name=self.userRolled.name, icon_url=self.userRolled.avatar.url)
+                await inter.response.send_message(embed=embedVar)
             except Exception as e:
                 traceback.print_exc()
                 embedVar = disnake.Embed(title="Ocorreu um erro", description="Se o erro persistir, contate Farrys.")
                 await inter.response.send_message(embed=embedVar, ephemeral=True)
             else:
-                if atributo != "Sanidade":
+                if self.atrOld != "Sanidade":
                     if extra == 0:
                         await self.answeringInter(inter, int(data), hasExtra= False)
                     else:
@@ -100,6 +105,11 @@ class Roll(commands.Cog):
         else:
             embedVar = disnake.Embed(title="Atributo inválido.", description="Verifique se digitou o atributo corretamente.")
             await inter.response.send_message(embed=embedVar, ephemeral=True)
+
+    @roll.autocomplete("atributo")
+    async def roll_autocomp(self, inter, string : str):
+        string = string.title()
+        return [atr for atr in varHolder.rollToAutocomp if string in atr.title()]
 
 def setup(bot):
     bot.add_cog(Roll(bot))
--- utils/varHolder.py
+++ utils/varHolder.py
@@ -7,3 +7,8 @@ atributos = ["Nome", "Idade", "Vida", "Sanidade", "Força", "Destreza", "Intelig
         "Espingarda", "Pistola", "Metralhadora", "Armas Elétricas", "Machado", "Facas", "Espadas", "Espadas Curtas", "Bastão", "Arco"]
 
 servers = ["1962", "Ceia de Sangue"]
+
+rollToAutocomp = ["Sanidade", "Força", "Destreza", "Inteligência", "Vitalidade", "Resistência",
+                "Detectar Mentiras", "Primeiros Socorros", "Sacar Rápido", "Armas Elétricas", "Espadas Curtas",
+                "Furtividade", "Lábia", "Encontrar", "Briga", "Esquiva", "Bloqueio", 
+                "Espada", "Pistola", "Faca"]
